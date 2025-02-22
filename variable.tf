bdb30e3b13dba24c4c042b2081f0bab5b597f2cc
krishna-gcp <156681083+krishna-gcp@users.noreply.github.com>
2025-02-22 22:19:54 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,11 @@
-variable "project_id" {
-  description = "The GCP project ID"
+variable "GOOGLE_CREDENTIALS" {
+  description = "The base64-encoded GCP service account key"
   type        = string
+  sensitive   = true
 }
 
 variable "region" {
-  description = "The GCP region"
+  description = "The GCP region to deploy resources"
   type        = string
+  default     = "us-central1"
 }
