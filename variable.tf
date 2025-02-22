1b607021dbfee58bb08e40b71a56c82659d6c67d
krishna-gcp <156681083+krishna-gcp@users.noreply.github.com>
2025-02-22 18:35:27 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,37 +1,8 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# REQUIRED PARAMETERS
-# ---------------------------------------------------------------------------------------------------------------------
-
-
-variable "project" {
-  description = "The project ID where all resources will be launched."
-  type        = string
+variable "project_id" {
+  description = "mydeployments-451712"
 }
 
-variable "location" {
-  description = "The location (region or zone)"
-  type        = string
-}
-
-variable "gcr_region" {
-  description = "Name of the GCP region where the GCR registry is located. e.g: 'us' or 'eu'."
-  type        = string
-}
-
-
-# ---------------------------------------------------------------------------------------------------------------------
-# OPTIONAL PARAMETERS
-# ---------------------------------------------------------------------------------------------------------------------
-
-
-variable "repository_name" {
-  description = "Name of the Google Cloud Source Repository to create."
-  type        = string
-  default     = "sample-docker-app"
-}
-
-variable "branch_name" {
-  description = "Example branch name used to trigger builds."
-  type        = string
-  default     = "master"
+variable "region" {
+  description = "GCP Region"
+  default     = "us-central1"
 }
