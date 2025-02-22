5a693718568975da95927fcdd4c1ad74073e5138
krishna-gcp <156681083+krishna-gcp@users.noreply.github.com>
2025-02-22 20:48:57 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,9 +7,3 @@ variable "region" {
   description = "The GCP region"
   type        = string
 }
-
-variable "credentials" {
-  description = "The GCP service account credentials"
-  type        = string
-  sensitive   = true
-}
